c709b29a525dc866f4097c173f2d2f5c00977b22
David <David@DaviddeMacBook-Pro-3.local>
2016-12-21 17:26:15 +0800

    add parameters getter

--- NetworkRequest/PagingEnabledRequest.swift
+++ NetworkRequest/PagingEnabledRequest.swift
@@ -14,11 +14,15 @@ public protocol PagingEnabledRequest {
     var perPage: Int { get }
     /// Page you are going to fetch
     var page: Int { get set }
+    /// Paramters to make a paging request.
+    var pagingParameters: [String : Any] { get }
 }
 
 extension PagingEnabledRequest {
     
     public var perPage: Int { return 25 }
+    
+    var pagingParameters: [String : Any] { return ["page": page, "per_page": perPage] }
     
     func checkHasNextPage<Response>(results: [Response]) -> Promise<(results: [Response], nextPage: Int?)> {
         if results.count < perPage {
